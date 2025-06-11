1d784cf6c043527171a75aa2878228afa42acd15
miya-algoma <miya@algomau.ca>
2025-06-11 23:05:41 +0000

    Fixed undefined array key error in App router when URL is empty

--- app/core/App.php
+++ app/core/App.php
@@ -8,64 +8,47 @@ class App {
     protected $params = [];
 
     public function __construct() {
-        if (isset($_SESSION['auth']) == 1) {
-            //$this->method = 'index';
+        if (isset($_SESSION['auth']) && $_SESSION['auth'] === 1) {
             $this->controller = 'home';
-        } 
+        }
 
-        // This will return a broken up URL
-        // it will be /controller/method
         $url = $this->parseUrl();
 
-        /* if controller exists in the URL, then go to it
-         * if not, then go to this->controller which is defaulted to home 
-         */
-
-        if (file_exists('app/controllers/' . $url[1] . '.php')) {
+        // ✅ FIX: check if $url[1] exists before using it
+        if (isset($url[1]) && file_exists('app/controllers/' . $url[1] . '.php')) {
             $this->controller = $url[1];
-
             $_SESSION['controller'] = $this->controller;
 
-            /* This is if we have a special URL in the index.
-             * For example, our apply page is public and in the index method
-             * We do not want the method to be login in this case, but instead index
-             * 
-             */
-            if (in_array($this->controller, $this->special_url)) { 
-              $this->method = 'index';
+            if (in_array($this->controller, $this->special_url)) {
+                $this->method = 'index';
             }
+
             unset($url[1]);
+        } elseif (!isset($url[1]) || empty($url[1])) {
+            // Allow root to load default controller/method
         } else {
             header('Location: /home');
-            die;
+            exit;
         }
 
         require_once 'app/controllers/' . $this->controller . '.php';
 
         $this->controller = new $this->controller;
 
-        // check to see if method is passed
-        // check to see if it exists
-        if (isset($url[2])) {
-            if (method_exists($this->controller, $url[2])) {
-                $this->method = $url[2];
-                $_SESSION['method'] = $this->method;
-                unset($url[2]);
-            }
+        if (isset($url[2]) && method_exists($this->controller, $url[2])) {
+            $this->method = $url[2];
+            $_SESSION['method'] = $this->method;
+            unset($url[2]);
         }
 
-        // This will rebase the params to a new array (starting at 0)
-        // if params exist
         $this->params = $url ? array_values($url) : [];
         call_user_func_array([$this->controller, $this->method], $this->params);		
     }
 
     public function parseUrl() {
         $u = "{$_SERVER['REQUEST_URI']}";
-        //trims the trailing forward slash (rtrim), sanitizes URL, explode it by forward slash to get elements
         $url = explode('/', filter_var(rtrim($u, '/'), FILTER_SANITIZE_URL));
-		unset($url[0]);
-		return $url;
+        unset($url[0]); // remove first empty element from leading slash
+        return $url;
     }
-
 }
