b21b40bee5a8ca7382fd94d199741ea61689150b
miya-algoma <miya@algomau.ca>
2025-06-11 21:45:16 +0000

    Displayed lockout and login errors in login view

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,29 +1,36 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-            </div>
-        </div>
-    </div>
+	<?php require_once 'app/views/templates/headerPublic.php' ?>
+	<main role="main" class="container">
+			<div class="page-header" id="banner">
+					<div class="row">
+							<div class="col-lg-12">
+									<h1>You are not logged in</h1>
+							</div>
+					</div>
+			</div>
 
-<div class="row">
-    <div class="col-sm-auto">
-		<form action="/login/verify" method="post" >
-		<fieldset>
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input required type="text" class="form-control" name="username">
+			<?php if (isset($_SESSION['auth_error'])): ?>
+					<div class="alert alert-danger">
+							<?= $_SESSION['auth_error']; unset($_SESSION['auth_error']); ?>
+					</div>
+			<?php endif; ?>
+
+			<div class="row">
+					<div class="col-sm-auto">
+							<form action="/login/verify" method="post">
+									<fieldset>
+											<div class="form-group">
+													<label for="username">Username</label>
+													<input required type="text" class="form-control" name="username">
+											</div>
+											<div class="form-group">
+													<label for="password">Password</label>
+													<input required type="password" class="form-control" name="password">
+											</div>
+											<br>
+											<button type="submit" class="btn btn-primary">Login</button>
+									</fieldset>
+							</form> 
+					</div>
 			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input required type="password" class="form-control" name="password">
-			</div>
-            <br>
-		    <button type="submit" class="btn btn-primary">Login</button>
-		</fieldset>
-		</form> 
-	</div>
-</div>
-    <?php require_once 'app/views/templates/footer.php' ?>
+	</main>
+	<?php require_once 'app/views/templates/footer.php' ?>
