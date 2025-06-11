55d5f97fb27573fe6b4e835dd1232c47a5cb48c9
miya-algoma <miya@algomau.ca>
2025-06-11 22:41:33 +0000

    Reports now showing in website

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,26 +1,19 @@
-<?php require_once 'app/views/templates/header.php' ?>
-<div class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
+    <?php require_once 'app/views/templates/header.php'; ?>
 
-                <pre>
-                <?php
-                    echo "Session username: ";
-                    var_dump($_SESSION['username']);
-                ?>
-                </pre>
-
-                <h1>Hey</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
+    <div class="container">
+        <div class="page-header" id="banner">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Hey</h1>
+                    <p class="lead"> <?= date("F jS, Y"); ?></p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <p> <a href="/logout">Click here to logout</a></p>
+        <div class="row">
+            <div class="col-lg-12">
+                <p> <a href="/logout">Click here to logout</a></p>
+            </div>
         </div>
-    </div>
 
-<?php require_once 'app/views/templates/footer.php' ?>
+    <?php require_once 'app/views/templates/footer.php'; ?>
